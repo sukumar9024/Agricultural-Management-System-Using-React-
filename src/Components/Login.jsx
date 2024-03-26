7b1db5b859acfe99cd61a45c7a17dc89dd54597f
Sukumar <sukumarchintham866@gmail.com>
2024-03-26 10:18:26 +0530

    Small Changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -12,41 +12,26 @@ function Login(){
     }
     return(
         
-<<<<<<< HEAD
-        <div class="login-cointainer" style={myStyle}>
-        <div class="customer">
-            <div class="customer-heading"><h2>Customer</h2></div>
-            <div class="customer-logo"><span class="material-symbols-outlined">
-                <img src={customer} style={{height: 50}}/>
-=======
         <div className="login-cointainer" style={myStyle}>
         <div className="customer">
             <div className="customer-heading"><h2>Customer</h2></div>
             <div className="customer-logo"><span className="material-symbols-outlined">
-                person_add
->>>>>>> 483bbef24e85c3f07d7e1b10b5cf2bced4f38acd
+                <img src={customer} style={{height:"10em"}}/>
                 </span></div>
             <div className="customer-para"><p>Are you willing to purchase products from farmers? </p></div>
             <div className="login-line">login/register as  Customer</div>
-            <div className="login_button"><Link to="/Customerlogin" ><button type="button">Login</button></Link></div>
-            <div className="login_button"><Link to="/Customerregister"><button type="button">Register</button></Link></div>
+            <div className="login_button"><Link to="/Customerlogin" ><button type="button" style={{height:"2em", width:"5em"}}>Login</button></Link></div>
+            <div className="login_button"><Link to="/Customerregister"><button type="button" style={{height:"2em", width:"5em"}}>Register</button></Link></div>
         </div>
-<<<<<<< HEAD
-        <div class="farmer">
-            <div class="farmer-heading"><h2>Farmer</h2></div>
-            <div class="farmer-logo"><span class="material-symbols-outlined">
-                <img src={farmer} style={{height: 50}}/>
-=======
         <div className="farmer">
             <div className="farmer-heading"><h2>Farmer</h2></div>
             <div className="farmer-logo"><span className="material-symbols-outlined">
-                grocery
->>>>>>> 483bbef24e85c3f07d7e1b10b5cf2bced4f38acd
+                <img src={farmer} style={{height:"10em"}}/>
                 </span></div>
             <div className="farmer-para"><p>Online Market where you can sell agri products. </p></div>
             <div className="login-line">login/register as  Farmer</div>
-            <div className="login_button"><Link to="/farmerlogin" ><button type="button">Login</button></Link></div>
-            <div className="login_button"><Link to="/farmerregister" ><button type="button">Register</button></Link></div>
+            <div className="login_button"><Link to="/farmerlogin" ><button type="button" style={{height:"2em", width:"5em"}}>Login</button></Link></div>
+            <div className="login_button"><Link to="/farmerregister" ><button type="button" style={{height:"2em", width:"5em"}}>Register</button></Link></div>
         </div>
         
     </div>
